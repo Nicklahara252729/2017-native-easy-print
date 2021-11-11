27b4aff17189bbd1327075f5f77810d35de00b50
Nics Projects <nicsprojects25@gmail.com>
2021-11-11 10:51:58 +0700

    it was born

---
+++ index.php
@@ -0,0 +1,80 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, height=device-height, user-scalable=no, initial-scale=1, maximum-scale=1, minium-scale=1">
+        <title>Easy Print</title>
+        <link href="css/bootstrap.css" rel="stylesheet">
+        <link href="css/jquery-ui.css" rel="stylesheet">
+        <link href="css/easy_print.css" rel="stylesheet">
+        <link href="material-icon/material-icon.css" rel="stylesheet">
+        <script src="js/jquery-2.1.4.js" type="text/javascript"></script>
+    </head>
+    <body>
+        <div class="navbar navbar-default">
+            <div class="navbar-inner">
+                <div class="container">
+                    <a href="index.php" class="brand"><span class="glyphicon glyphicon-print"></span> &nbsp; EasyPrint</a>
+                </div>
+            </div>
+        </div>
+        <div class="jumbotron">
+            <div class="container">
+                <div class="rows">
+                    <div id="mySlide1" class="carousel slide">
+                        <div class="rows">
+                            <div class="carousel-inner">
+                            <div class="item active col-md-12" id="txt-ban">
+                                <span class="fn-50 wolf"> EasyPrint</span><br>
+                                <span class="fn-15">nico gawa lahara nico gawa lahara nico gawa lahara nico gawa lahara nico gawa lahara nico gawa lahara nico gawa lahara
+                                nico gawa lahara nico gawa lahara nico gawa lahara nico gawa lahara nico gawa       lahara nico gawa lahara nico gawa lahara
+                                nico gawa lahara nico gawa lahara nico gawa lahara nico gawa lahara nico gawa       lahara nico gawa lahara nico gawa lahara
+                                nico gawa lahara nico gawa lahara nico gawa lahara nico gawa lahara nico gawa       lahara nico gawa lahara nico gawa lahara</span>
+                                nico gawa lahara nico gawa lahara nico gawa lahara nico gawa lahara nico gawa       lahara nico gawa lahara nico gawa lahara
+                                nico gawa lahara nico gawa lahara nico gawa lahara nico gawa lahara nico gawa       lahara nico gawa lahara nico gawa lahara
+                                nico gawa lahara nico gawa lahara nico gawa lahara nico gawa lahara nico gawa       lahara nico gawa lahara nico gawa lahara
+                            </div>
+                            <div class="item col-md-12" id="txt-ban">
+                                <span class="fn-50 wolf"> EasyPrint</span><br>
+                                <span class="fn-15">nico gawa lahara nico gawa lahara nico gawa lahara nico gawa lahara nico gawa lahara nico gawa lahara nico gawa lahara
+                                nico gawa lahara nico gawa lahara nico gawa lahara nico gawa lahara nico gawa       lahara nico gawa lahara nico gawa lahara
+                                nico gawa lahara nico gawa lahara nico gawa lahara nico gawa lahara nico gawa       lahara nico gawa lahara nico gawa lahara
+                                nico gawa lahara nico gawa lahara nico gawa lahara nico gawa lahara nico gawa       lahara nico gawa lahara nico gawa lahara</span>
+                                nico gawa lahara nico gawa lahara nico gawa lahara nico gawa lahara nico gawa       lahara nico gawa lahara nico gawa lahara
+                                nico gawa lahara nico gawa lahara nico gawa lahara nico gawa lahara nico gawa       lahara nico gawa lahara nico gawa lahara
+                                nico gawa lahara nico gawa lahara nico gawa lahara nico gawa lahara nico gawa       lahara nico gawa lahara nico gawa lahara
+                            </div>
+                            <div class="item col-md-12" id="txt-ban">
+                                <span class="fn-50 wolf"> EasyPrint</span><br>
+                                <span class="fn-15">nico gawa lahara nico gawa lahara nico gawa lahara nico gawa lahara nico gawa lahara nico gawa lahara nico gawa lahara
+                                nico gawa lahara nico gawa lahara nico gawa lahara nico gawa lahara nico gawa       lahara nico gawa lahara nico gawa lahara
+                                </span>
+                            </div>
+                        </div>
+                        </div>
+                        <div class="rows">
+                            <ol>
+                            <li data-target="#mySlide1" data-slide-to="0" class="active"></li>
+                            <li data-target="#mySlide1" data-slide-to="1" ></li>
+                            <li data-target="#mySlide1" data-slide-to="2" ></li>
+                        </ol>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="rows">
+                
+            </div>
+        </div>
+        <script src="js/jquery.js" type="text/javasciprt"></script>
+        <script src="js/bootstrap.min.js" type="text/javascript"></script>
+        <script src="js/jquery-ui.js" type="text/javascript"></script>
+        <script type="text/javascript" charset="utf-8"> 
+			$(document).ready(function(){
+				$('#mySlide1').carousel();
+			});
+		</script>
+    </body>
+</html>
